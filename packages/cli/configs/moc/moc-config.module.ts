87002d96b9cfa9f1cfdd52fc932b45787898aef3
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2021-09-26 16:38:41 +0300

    style(config): change modules file system

--- packages/cli/configs/moc/moc-config.module.ts
+++ packages/cli/configs/moc/moc-config.module.ts
@@ -1,25 +1,7 @@
 import { AbstractConfigModule } from '../../src/interfaces';
 
-import { BranchNameLintModule } from './branch-name-lint/branch-name-lint.module';
-import { CommitLintModule } from './commitlint/commitlint.module';
-import { EditorConfigModule } from './editorconfig/editorconfig.module';
-import { EslintModule } from './eslint/eslint.module';
-import { HuskyModule } from './husky/husky.module';
-import { LintStagedModule } from './lint-staged/lint-staged.module';
-import { PrettierModule } from './prettier/prettier.module';
-import { SmileTrackModule } from './smile-track/smile-track.module';
-import { StylelintModule } from './stylelint/stylelint.module';
+import { modules } from './modules';
 
 export class ConfigModule implements AbstractConfigModule {
-  modules = [
-    BranchNameLintModule,
-    CommitLintModule,
-    EditorConfigModule,
-    EslintModule,
-    HuskyModule,
-    LintStagedModule,
-    PrettierModule,
-    SmileTrackModule,
-    StylelintModule,
-  ];
+  modules = modules;
 }
---
+++ packages/cli/configs/moc/modules/index.ts
@@ -0,0 +1,31 @@
+import { BranchNameLintModule } from './branch-name-lint/branch-name-lint.module';
+import { CommitLintModule } from './commitlint/commitlint.module';
+import { EditorConfigModule } from './editorconfig/editorconfig.module';
+import { EslintModule } from './eslint/eslint.module';
+import { HuskyModule } from './husky/husky.module';
+import { LintStagedModule } from './lint-staged/lint-staged.module';
+import { PrettierModule } from './prettier/prettier.module';
+import { SmileTrackModule } from './smile-track/smile-track.module';
+import { StylelintModule } from './stylelint/stylelint.module';
+
+export const modules = [
+  BranchNameLintModule,
+  CommitLintModule,
+  EditorConfigModule,
+  EslintModule,
+  HuskyModule,
+  LintStagedModule,
+  PrettierModule,
+  SmileTrackModule,
+  StylelintModule,
+];
+
+export * from './branch-name-lint/branch-name-lint.module';
+export * from './commitlint/commitlint.module';
+export * from './editorconfig/editorconfig.module';
+export * from './eslint/eslint.module';
+export * from './husky/husky.module';
+export * from './lint-staged/lint-staged.module';
+export * from './prettier/prettier.module';
+export * from './smile-track/smile-track.module';
+export * from './stylelint/stylelint.module';
