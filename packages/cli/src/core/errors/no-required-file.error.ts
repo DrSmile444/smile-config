ea6b51b41bab48d74d068cfe5ba7dad5543a9b8d
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2021-10-02 12:05:56 +0300

    fix(core): added missing file name to NoRequiredFileError

--- packages/cli/src/core/errors/no-required-file.error.ts
+++ packages/cli/src/core/errors/no-required-file.error.ts
@@ -1,6 +1,6 @@
 export class NoRequiredFileError extends Error {
   name = 'NoRequiredFileError';
-  message = 'There is no config required file in the folder';
+  message = 'There is no config required file in the folder: "' + this.requiredFile + '"';
 
   constructor(public requiredFile: string) {
     super();
